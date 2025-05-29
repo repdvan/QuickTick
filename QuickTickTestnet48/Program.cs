e9f12ab7b90fb2fc413285e0911e2bfff68ef643
repdvan <repdvan@github.com>
2025-05-29 09:12:04 -0500

    Add simple console projects for testing .NET 8.0 and .NET Framework 4.8

---
+++ QuickTickTestnet48/Program.cs
@@ -0,0 +1,28 @@
+﻿using QuickTickLib;
+using System;
+using System.Threading;
+
+namespace QuickTickTestnet48
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            using QuickTickTimer timer = new(1000.0 / 60.0);
+            timer.AutoReset = true;
+            timer.Elapsed += Timer_Elapsed;
+            timer.Start();
+
+            Thread.Sleep(1000); // Run for 5 seconds
+            timer.Stop();
+        }
+
+        private static void Timer_Elapsed(object? sender, QuickTickElapsedEventArgs e)
+        {
+            var diff = e.SignalTime - e.ScheduledTime;
+            Console.WriteLine($"Timer elapsed! Scheduled: {e.ScheduledTime:dd.MM.yyyy HH:mm:ss.fff}, Actual: {e.SignalTime:dd.MM.yyyy HH:mm:ss.fff} ms diff: {diff.TotalMilliseconds}");
+        }
+    }
+}
---
+++ QuickTickTestnet8/Program.cs
@@ -0,0 +1,26 @@
+﻿using QuickTickLib;
+
+namespace QuickTickTestnet8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            using QuickTickTimer timer = new(1000.0 / 60.0);
+            timer.AutoReset = true;
+            timer.Elapsed += Timer_Elapsed;
+            timer.Start();
+
+            Thread.Sleep(1000); // Run for 5 seconds
+            timer.Stop();
+        }
+
+        private static void Timer_Elapsed(object? sender, QuickTickElapsedEventArgs e)
+        {
+            var diff = e.SignalTime - e.ScheduledTime;
+            Console.WriteLine($"Timer elapsed! Scheduled: {e.ScheduledTime:dd.MM.yyyy HH:mm:ss.fff}, Actual: {e.SignalTime:dd.MM.yyyy HH:mm:ss.fff} ms diff: {diff.TotalMilliseconds}");
+        }
+    }
+}
